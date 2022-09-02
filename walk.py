1a232e9c3786426bf1e37b172393d25192bc369a
Xiaoyu Liu <feliconut@outlook.com>
2022-09-01 21:50:51 -0400

    refactor: change default_edge_probability() to EdgeProperty.default()

--- walk.py
+++ walk.py
@@ -5,73 +5,89 @@ from typing import Dict
 
 class RandomWalk():
     class EdgeProperty():
-        '''Sometimes we give each edge some property that may be subject to change.
+        '''
+        Sometimes we give each edge some property that may be subject to change.
+
         Examples of edge properties are:
         1. In simple random walk there is no edge property
         2. In Edge Reinforced RW we need to store the number of time each edge is visited
         '''
+        @classmethod
+        def default(cls, position: int):
+            return None
 
     class Step():
-        '''Describes the change to system state when one action is taken.
+        '''
+        Describes the change to system state when one action is taken.
+
         weight: Used to determine probability to choosing this step
         age: system age after this step
         modified_edges: change to edge properties after this step
-        position: new position after this step'''
+        position: new position after this step
+        '''
 
         def __init__(self, weight: float, t: int, modified_edges: dict, position: int) -> None:
-            self.weight = weight
-            self.t = t
+            self.weight: float = weight
+            self.t: int = t
             self.modified_edges: Dict[int,
                                       RandomWalk.EdgeProperty] = modified_edges
-            self.position = position
+            self.position: int = position
 
-    def __init__(self, random_seed: str = None):
-        self.t = 0  # steps of simulation so far
-        self.modified_edges = dict()  # current block properties that are not default
-        self.position = 0  # current step of the walker
-        self.random_seed = random_seed  # used to make pseudoranodm choices.
+    def __init__(self, _random_seed: str = None):
+        # steps of simulation so far
+        self.t: int = 0
+        # current edge properties that are not default
+        self.modified_edges: Dict[int, RandomWalk.EdgeProperty] = dict()
+        # current step of the walker
+        self.position: int = 0
+        # used to make pseudoranodm choices.
+        self._random_seed: str = _random_seed
 
-    def default_edge_property(self, n) -> EdgeProperty:
-        return self.__class__.EdgeProperty()
+    def __reset_random(self):
+        if self._random_seed is not None:
+            random.seed(self._random_seed)
 
-    def get_edge_property(self, n: int) -> EdgeProperty:
-        if n in self.modified_edges:
-            return self.modified_edges[n]
+    def get_edge_property(self, position: int) -> EdgeProperty:
+        if position in self.modified_edges:
+            return self.modified_edges[position]
         else:
-            return self.default_edge_property(n)
+            return self.__class__.EdgeProperty.default(position)
 
     def choose_step(self) -> Step:
-        # get moves
+        # get steps
         steps = list(self.available_steps())
         steps.sort(key=lambda x: x.position)
 
-        # choose a move
-        random.seed(self.random_seed)
+        # choose a step
+        self.__reset_random()
         step = random.choices(steps, weights=[x.weight for x in steps])[0]
 
         return step
 
-    def take_step(self, move: Step):
+    def take_step(self, step: Step):
         # update
-        self.t = move.t
-        self.modified_edges.update(move.modified_edges)
-        self.position = move.position
+        self.t = step.t
+        self.modified_edges.update(step.modified_edges)
+        self.position = step.position
 
     def available_steps(self):
         raise NotImplementedError()
 
 
 class SimpleSymmetricRandomWalk(RandomWalk):
+    '''
+    In this type of random walk, the walker walk left or right with equal probability.
+    '''
+
     def available_steps(self):
         Step = RandomWalk.Step
-        n = self.position
-        for i in [-1, 1]:  # relative motions
-            yield Step(
+        yield from [
+            Step(
                 weight=1,
                 t=self.t + 1,
-                modified_edges=dict(),  # no block property to update
-                position=n+i
-            )
+                modified_edges=dict(),  # no edge property
+                position=position
+            ) for position in [self.position-1, self.position+1]]
 
 
 class SimpleAsymmetricRandomWalk(RandomWalk):
@@ -79,11 +95,13 @@ class SimpleAsymmetricRandomWalk(RandomWalk):
 
 
 class LinearEdgeReinforcedRandomWalk(RandomWalk):
-    class BlockProperty(RandomWalk.EdgeProperty, int):
-        pass
-
-    def default_edge_property(self, n) -> BlockProperty:
-        return self.__class__.BlockProperty(1)
+    class EdgeProperty(RandomWalk.EdgeProperty, int):
+        '''
+        This is the number of times this edge is visited.
+        '''
+        @classmethod
+        def default(cls, position: int):
+            return cls(1)
 
     def available_steps(self):
         Step = RandomWalk.Step
