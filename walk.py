05018e41726b33a887a73a6e45f4a57ada4a28dd
Xiaoyu Liu <feliconut@outlook.com>
2022-09-01 22:05:06 -0400

    feat: class SimpleAsymmetricRandomWalk

--- walk.py
+++ walk.py
@@ -6,11 +6,11 @@ from typing import Dict
 class RandomWalk():
     class EdgeProperty():
         '''
-        Sometimes we give each edge some property that may be subject to change.
+            Sometimes we give each edge some property that may be subject to change.
 
-        Examples of edge properties are:
-        1. In simple random walk there is no edge property
-        2. In Edge Reinforced RW we need to store the number of time each edge is visited
+            Examples of edge properties are:
+            1. In simple random walk there is no edge property
+            2. In Edge Reinforced RW we need to store the number of time each edge is visited
         '''
         @classmethod
         def default(cls, position: int):
@@ -18,12 +18,12 @@ class RandomWalk():
 
     class Step():
         '''
-        Describes the change to system state when one action is taken.
+            Describes the change to system state when one action is taken.
 
-        weight: Used to determine probability to choosing this step
-        age: system age after this step
-        modified_edges: change to edge properties after this step
-        position: new position after this step
+            weight: Used to determine probability to choosing this step
+            age: system age after this step
+            modified_edges: change to edge properties after this step
+            position: new position after this step
         '''
 
         def __init__(self, weight: float, t: int, modified_edges: dict, position: int) -> None:
@@ -79,7 +79,7 @@ class RandomWalk():
 
 class SimpleSymmetricRandomWalk(RandomWalk):
     '''
-    In this type of random walk, the walker walk left or right with equal probability.
+        In this type of random walk, the walker walk left or right with equal probability.
     '''
 
     def available_steps(self):
@@ -94,14 +94,36 @@ class SimpleSymmetricRandomWalk(RandomWalk):
 
 
 class SimpleAsymmetricRandomWalk(RandomWalk):
-    def __init__(self, _random_seed: bytes = None):
-        super().__init__()
+    '''
+        In this type of random walk, the walker walk left or right with probability 1-p and p respectively.
+    '''
+
+    def __init__(self, p: float = 0.5, _random_seed: bytes = None):
+        super().__init__(_random_seed)
+        self.p: float = p
+
+    def available_steps(self):
+        Step = RandomWalk.Step
+        yield from [
+            Step(
+                weight=1-self.p,
+                t=self.t + 1,
+                modified_edges=dict(),  # no edge property
+                position=self.position-1
+            ),
+            Step(
+                weight=self.p,
+                t=self.t + 1,
+                modified_edges=dict(),  # no edge property
+                position=self.position+1
+            )
+        ]
 
 
 class LinearEdgeReinforcedRandomWalk(RandomWalk):
     class EdgeProperty(RandomWalk.EdgeProperty, int):
         '''
-        This is the number of times this edge is visited.
+            This is the number of times this edge is visited.
         '''
         @classmethod
         def default(cls, position: int):
@@ -129,7 +151,7 @@ class LinearEdgeReinforcedRandomWalk(RandomWalk):
 
 # %%
 if __name__ == "__main__":
-    rw = LinearEdgeReinforcedRandomWalk()
+    rw = SimpleAsymmetricRandomWalk(0.8)
     for i in range(20):
         step = rw.choose_step()
         rw.take_step(step)
