bc93fdd2fcfe4525071a9ecf4105e36b8f013523
Lei <leifu1128@gmail.com>
2022-09-05 22:53:01 -0400

    Create generate_walk.py

---
+++ generate_walk.py
@@ -0,0 +1,33 @@
+import walk
+import pandas as pd
+
+
+class Experiment():
+    def __init__(self, walk):
+        self.walk = walk
+        self.data = pd.DataFrame()
+
+    def generate_walk(self, length):
+        trial = []
+
+        for i in range(length):
+            step = self.walk.choose_step()
+            self.walk.take_step(step)
+            trial.append(int(step.position))
+
+        return trial
+
+    def generate(self, trials :int, length :int):
+        for j in range(trials):
+            self.data[str(j + 1)] = self.generate_walk(length)
+
+    def test(self):
+        pass
+
+    def plot(self, num_trial=None):
+        if num_trial is None:
+            self.data.plot(legend=True)
+
+        else:
+            self.data.plot(x=num_trial, legend=True)
+            
