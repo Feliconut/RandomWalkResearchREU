f34d55ddd560c2c97e9fa339201b445ac5659f7d
Xiaoyu Liu <feliconut@outlook.com>
2022-09-06 16:48:48 -0400

    fix: Multiple Experiments

--- experiment.py
+++ experiment.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, Callable, List
 import walk
 import pandas as pd
 
@@ -8,7 +8,7 @@ class SingleExperiment():
         self.walk = walk
         self.data = pd.DataFrame()
 
-    def generate_walk(self, length: int) -> List[int]:
+    def run(self, length: int) -> List[int]:
         path = []
 
         for i in range(length):
@@ -16,15 +16,42 @@ class SingleExperiment():
             self.walk.take_step(step)
             path.append(float(step.position))
 
+        self.data[1]=path
+
+    def plot(self, ):
+        self.data.plot(legend=True)
+
+
+class MultipleExperiment():
+    def __init__(self, walk_cls: type, n_trials: int = None, length:int = None, *args, **kwargs):
+        self.walk_cls = walk_cls
+        self.n_trials = n_trials
+        self.length = length
+        self.data = pd.DataFrame()
+        self.args = args
+        self.kwargs = kwargs
+    
+    def new_walk(self) -> walk.RandomWalk:
+        return self.walk_cls(*self.args, **self.kwargs)
+
+    def run(self):
+        for i in range(self.n_trials):
+            path = self.single_walk()
+            self.data[i] = path
+
+    def single_walk(self) -> List[int]:
+        path = []
+
+        walk = self.new_walk()
+        for _ in range(self.length):
+            step = walk.choose_step()
+            walk.take_step(step)
+            path.append(float(step.position))
+
         return path
 
-    def generate(self, n_trials: int, length: int):
-        for j in range(n_trials):
-            self.data[j + 1] = self.generate_walk(length)
-
-    def plot(self, n_trials: int = None):
-        if n_trials is None:
-            self.data.plot(legend=True)
-
-        else:
-            self.data.plot(y=n_trials, legend=True)
+    def plot(self, trial_indices: List[int]):
+        self.data[trial_indices].plot(
+            legend=True,
+            title=f"{self.walk_cls.__name__}({self.n_trials} trials, {self.length} steps)",
+        )
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 # %%
 import walk
-from experiment import SingleExperiment
+from experiment import MultipleExperiment, SingleExperiment
 # %%
 test = SingleExperiment(walk.SimpleAsymmetricRandomWalk(0.8))
-test.generate(5, 20)
+test.run(20)
 
 # %%
 test.data
 # %%
-test.plot(n_trials=[1, 2, 3, 4, 5])
+test.plot()
+
+# %%
+test = MultipleExperiment(
+    walk.LinearEdgeReinforcedRandomWalk, 
+    n_trials = 10, 
+    length = 100000)
+test.run()
+test.plot([0,1,2,3,4,5,6,7,8,9])
 
 # %%
