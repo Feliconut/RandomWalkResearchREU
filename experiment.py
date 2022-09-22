256b968c7c75aa978f4e33304eb8736b6893e99a
Lei <leifu1128@gmail.com>
2022-09-22 16:33:39 -0400

    Update

--- experiment.py
+++ experiment.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy import stats
 from matplotlib import pyplot as plt
 
+
 class SingleExperiment():
     def __init__(self, walk: walk.RandomWalk):
         self.walk = walk
@@ -18,7 +19,7 @@ class SingleExperiment():
             self.walk.take_step(step)
             path.append(float(step.position))
 
-        self.data[1]=path
+        self.data[1] = path
 
     def plot(self, ):
         self.data.plot(legend=True)
@@ -29,14 +30,14 @@ class SingleExperiment():
 
 
 class MultipleExperiment():
-    def __init__(self, walk_cls: type, n_trials: int = None, length:int = None, *args, **kwargs):
+    def __init__(self, walk_cls: type, n_trials: int = None, length: int = None, *args, **kwargs):
         self.walk_cls = walk_cls
         self.n_trials = n_trials
         self.length = length
         self.data = None
         self.args = args
         self.kwargs = kwargs
-    
+
     def new_walk(self) -> walk.RandomWalk:
         return self.walk_cls(*self.args, **self.kwargs)
 
@@ -60,17 +61,21 @@ class MultipleExperiment():
         return path
 
     def plot(self, trial_indices: List[int]):
-         self.data[trial_indices].plot(
+        self.data[trial_indices].plot(
             legend=True,
             title=f"{self.walk_cls.__name__}({self.n_trials} trials, {self.length} steps)",
-            alpha = 0.7,
-            figsize = (20, 6)
+            alpha=0.7,
+            figsize=(20, 6)
         )
 
-
     def test(self):
-        statistic, pvalue = stats.normaltest(self.data.iloc[-1,:])
-        print(f"Statistic: {statistic}, p-value: {pvalue}")
+        print(f"Test Results for {self.n_trials} trials of length {self.length}")
+        statistic, pvalue = stats.normaltest(self.data.iloc[-1, :])
+        print(f"(K-Squared) Statistic: {statistic}, p-value: {pvalue}")
+        statistic, pvalue = stats.chisquare(self.data.iloc[-1, :])
+        print(f"(Chi-Square) Statistic: {statistic}, p-value: {pvalue}")
+        statistic, pvalue = stats.kstest(self.data.iloc[-1, :], stats.norm.cdf)
+        print(f"(K-S) Statistic: {statistic}, p-value: {pvalue}")
 
     def hist_plot(self):
-        self.data.iloc[-1,:].hist()
+        self.data.iloc[-1, :].hist()
