7ac2178506ee6b6cb3b78d93df8e6a687e175d3f
Xiaoyu Liu <feliconut@outlook.com>
2022-09-22 16:01:51 -0400

    p-value heatmap

--- project_log.md
+++ project_log.md
@@ -4,9 +4,9 @@
 
 Tasks due before next meeting:
 
-- [ ] (possibly) vectorize the path generating code, make it faster
+- [x] (possibly) vectorize the path generating code, make it faster
 - [ ] Learn about K-S test and Chi-squared test, and implement in the code
-- [ ] Tune the experiment parameters (trial length, number of trials) and look for trends
+- [x] Tune the experiment parameters (trial length, number of trials) and look for trends
 - [ ] Read about Brownian Motion and understand its defining properties
 - [ ] Design tests to determine if the data is Brownian Motion
 - [ ] Implement these tests and apply to SSRW
--- ssrw_batch.py
+++ ssrw_batch.py
@@ -1,15 +1,60 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-# %%
-n = 100
+from scipy import stats
+# %% Simple Random Walk
+n = 1000
 p = 0.5
 A = np.random.rand(n, n) > p
-# %%
 W = np.cumsum(
     (A.astype('int') - 
     (~A).astype('int')
     ).flatten())
-# %%
 plt.plot(W)
+
+stats.normaltest(W)
+# %% Simple Random Walk, slice normal test
+n = 1000
+length = 1000
+p = 0.5
+A = np.random.rand(n, length, 5) > p
+W = np.sum(
+    (A.astype('int') - 
+    (~A).astype('int')
+    ), axis=1)
+*_, pvalue = stats.normaltest(W)
+# %% p value for different n and length
+def pvalue(n, length, p):
+    print(f"n = {n}, length = {length}")
+    n, length = int(10**n), int(10**length)
+    A = np.random.rand(n, length, 10) > p
+    W = np.sum(
+        (A.astype('int') - 
+        (~A).astype('int')
+        ), axis=1)
+    return stats.normaltest(W)[-1].mean()
+x = np.linspace(1, 4.5,15)
+y = np.linspace(1, 4.5,15)
+X, Y = np.meshgrid(x, y)
+# %%
+Z = np.vectorize(pvalue)(X, Y, 0.5)
+np.save('pvalue.npy', Z)
+print('saved')
+plt.pcolormesh(X, Y, Z)
+plt.colorbar()
+plt.xlabel('log10(n)')
+plt.ylabel('log10(length)')
+plt.title('p-value for normal test')
+
+# %%
+Z = np.load('pvalue.npy')
+# %%
+Z.shape
+# %%
+plt.pcolormesh(X, Y, Z)
+plt.colorbar()
+plt.xlabel('log10(n)')
+plt.ylabel('log10(length)')
+plt.title('p-value for normal test')
+
 # %%
