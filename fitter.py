f198539fb03440fbe666cac021ba5ce26cbc07f0
Lei <leifu1128@gmail.com>
2023-01-24 13:56:32 -0500

    Update

--- fitter.py
+++ fitter.py
@@ -9,23 +9,37 @@ class BetaFitter:
     def __init__(self, data):
         self.og_data = data
         self.data = OrderedDict(sorted(Counter(data).items()))
-        self.x = np.asarray(list(self.data.keys()), dtype=np.float32)[1:-1]
-        self.y = np.asarray(list(self.data.values()), dtype=np.float32)[1:-1]
+        self.x = np.asarray(list(self.data.keys()), dtype=np.float32)
+        self.y = np.cumsum(np.asarray(list(self.data.values()), dtype=np.float32) / len(data))
 
+        if self.x[0] == 0:
+            self.y[0] = 0
+
+        if self.x[-1] == 1:
+            self.y[-1] = 1
+
+        self.output = None
         self.params = None
         self.params_cov = None
+        self.last_guess = None
 
     @staticmethod
     def func_beta(x, a):
-        return scipy.stats.beta.pdf(x, a, a)
+        return scipy.stats.beta.cdf(x, a, a)
+
+    def func_beta_residuals(self, a):
+        return self.y - self.func_beta(self.x, a)
 
     def fit(self, p0, method):
-        self.params, self.params_cov = scipy.optimize.curve_fit(self.func_beta, self.x, self.y, p0,
-                                                                method=method)
+        self.last_guess = p0[0]
+        self.output = scipy.optimize.least_squares(self.func_beta_residuals, p0, method=method)
+        self.params = self.output.x
         print(self.params)
+        print(self.output.message)
 
     def plot(self, bins):
-        plt.hist(self.og_data, bins=bins, density=True)
+        plt.plot(self.x, self.y)
         plt.plot(self.x, self.func_beta(self.x, *self.params), label='fit')
+        plt.plot(self.x, self.func_beta(self.x, self.last_guess), label='guess')
         plt.legend(loc='best')
         plt.show()
